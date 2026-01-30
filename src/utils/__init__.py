c6e4ec54949cf681364b3d1b5aa377c693dbf267
Peeyush Patel <patelpeeyush2000@gmail.com>
2026-01-30 14:15:54 -0800

    gemini_chat stuff added

---
+++ src/utils/__init__.py
@@ -0,0 +1,16 @@
+"""Utility functions for Lychee-prompter."""
+
+from .gemini_chat import (
+    load_gemini_key,
+    init_gemini_client,
+    GeminiChat,
+    chat_with_gemini
+)
+
+__all__ = [
+    "load_gemini_key",
+    "init_gemini_client",
+    "GeminiChat",
+    "chat_with_gemini"
+]
+
